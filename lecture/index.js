c7f23756e0e8782a35d83ff889bf5f9006a1b178
bangms <uubang@knou.ac.kr>
2023-01-31 10:01:03 +0900

    xlsx 패티키로 엑셀 파싱하기

--- lecture/index.js
+++ lecture/index.js
@@ -1,3 +1,4 @@
+/*
 const parse = require('csv-parse/lib/sync');
 const fs = require('fs'); // 파일 시스템 모듈 (파일을 읽어와야 하기 때문에)
 
@@ -9,3 +10,33 @@ const records = parse(csv.toString('utf-8')); // csv-parse의 parse 메서드가
 records.forEach((r, i) => {
     console.log(i, r);
 })
+*/
+
+// 엑셀을 파싱하는게 힘들기 때문에  xslx 패키지 설치
+
+const xlsx = require('xlsx');
+
+const workbook = xlsx.readFile('xlsx/data.xlsx');
+
+// console.log(workbook);
+// console.log(Object.keys(workbook.Sheets));
+console.log(workbook.Sheets.영화목록);
+const ws = workbook.Sheets.영화목록;
+
+const records = xlsx.utils.sheet_to_json(ws);
+console.log(records);
+
+// forEach 나 for문 두가지 다 잘 쓰임
+// 자바스크립트는 싱글스레드이기 때문에 (비동기) 크롤링을 할 때에도 똑같이 적용
+// 크롤링 할 때도 비동기를 조심하면서 해야 함 두가지의 차이가 극명함 
+// 비동기를 자유자재로 다룰 수 있어야 크롤링 된 데이터들을 한번에 모아서 저장할 수 있는데 그 부분을 유의하기 
+
+records.forEach((r, i) => {
+    console.log(r.제목, r.링크)
+})
+
+// 배열.entries()를 쓰면 내부 배열이 [인덱스, 값] 모양 이터레이터로 바뀜
+// 2차원 배열로 변환해서 비구조화 할당을 해서 사용
+for (const [i, r] of records.entries()) {
+    console.log(i, r.제목, r.링크);
+}
