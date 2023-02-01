c8021210e345a6bf773ad597d705e51e238ef413
bangms <uubang@knou.ac.kr>
2023-02-01 10:53:11 +0900

    크롤링 결과물 다시 엑셀에 입력하기

--- lecture/index.js
+++ lecture/index.js
@@ -17,24 +17,25 @@ records.forEach((r, i) => {
 const xlsx = require('xlsx');
 const axios = require('axios'); // ajax 라이브러리
 const cheerio = require('cheerio'); // html 파싱
+const add_to_sheet = require('./add_to_sheet');
 
 const workbook = xlsx.readFile('xlsx/data.xlsx');
 console.log(workbook.SheetNames); // 시트 제목 가져오기
-for (const name of workbook.SheetNames) {
-    const ws = workbook.Sheets[name];
-    // 시트별로 따로 코딩
-}
+// for (const name of workbook.SheetNames) {
+//     const ws = workbook.Sheets[name];
+//     // 시트별로 따로 코딩
+// }
 
 // console.log(workbook);
 // console.log(Object.keys(workbook.Sheets));
 // console.log(workbook.Sheets.영화목록);
-// const ws = workbook.Sheets.영화목록;
+const ws = workbook.Sheets.영화목록;
 
-// const records = xlsx.utils.sheet_to_json(ws);
+const records = xlsx.utils.sheet_to_json(ws);
 // r.제목 r.링크가 아니라 r.A, r.B 이런식으로 가져오는 방법
-ws['!ref'] = 'A2:B11'; // 이렇게 걍 바꿔줘도 됨
+// ws['!ref'] = 'A2:B11'; // 이렇게 걍 바꿔줘도 됨
 // console.log(ws['!ref']);
-const records = xlsx.utils.sheet_to_json(ws, { header: 'A' }); 
+// const records = xlsx.utils.sheet_to_json(ws, { header: 'A' }); 
 // 엑셀 컬럼명이 속성명으로 들어감 대신 첫번째 제목까지 포함되어서 들어옴
 // console.log(ws['!ref']); // A1:B11 엑셀에서 읽어들여오는 부분 -> A2:B11 로 변경하면 됨
 // 엑셀에서 파싱해 올 범위를 수정해주면 제목 제외하고 받아올 수 있음
@@ -43,7 +44,7 @@ const records = xlsx.utils.sheet_to_json(ws, { header: 'A' });
 //     return v;
 // }).join(':');
 // records.shift(); // 맨 위에 제거
-console.log(records);
+// console.log(records);
 
 // forEach 나 for문 두가지 다 잘 쓰임
 // 자바스크립트는 싱글스레드이기 때문에 (비동기) 크롤링을 할 때에도 똑같이 적용
@@ -62,20 +63,24 @@ console.log(records);
 
 // axios cheerio 조합
 
-// const crawler = async () => { // await 쓰기 위해 async 사용
+const crawler = async () => { // await 쓰기 위해 async 사용
+    add_to_sheet(ws, 'C1', 's', '평점');
 //     // await Promise.all(records.map(async (r) => {
-//         for (const [i, r] of records.entries()) {
-        
-//             const response = await axios.get(r.링크); // 가져오는 요청을 보냄 axios.get
-//             if (response.status === 200) { // 응답이 성공했을 경우
-//                 const html = response.data;
-//                 // console.log(html); // html 코드를 가져오기
-//                 const $ = cheerio.load(html); // html 문자열이 cheerio에 로딩됨 -> $ 를 통해서 html 태그에 접근할 수 있음
-//                 const text = $('.score.score_left .star_score').text();
-                
-//                 console.log(r.제목, '평점', text.trim()); // trim으로 공백문자열 지우기
-//             }
-//         }
+    for (const [i, r] of records.entries()) {
+    
+        const response = await axios.get(r.링크); // 가져오는 요청을 보냄 axios.get
+        if (response.status === 200) { // 응답이 성공했을 경우
+            const html = response.data;
+            // console.log(html); // html 코드를 가져오기
+            const $ = cheerio.load(html); // html 문자열이 cheerio에 로딩됨 -> $ 를 통해서 html 태그에 접근할 수 있음
+            const text = $('.score.score_left .star_score').text();
+            
+            console.log(r.제목, '평점', text.trim()); // trim으로 공백문자열 지우기
+            const newCell = 'C' + (i + 2); // C2부터 C11까지
+            add_to_sheet(ws, newCell, 'n', parseFloat(text.trim())); // 시트 객체에다 숫자를 넣어준 것
+        }
+    }
+    xlsx.writeFile(workbook, 'xlsx/result.xlsx');
 //     // }));
 //     /*
 //     엑셀에 적힌 순서
@@ -103,6 +108,6 @@ console.log(records);
 //     대신 순서대로 요청을 보내고 응답을 받기 때문에 속도가 좀 느림
 //     둘다 간단한 페이지는 axios와 cheerio로 가능함
 //     */
-// }
+}
 
-// crawler();
+crawler();
