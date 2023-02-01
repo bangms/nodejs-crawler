267fe21394db37cf6ae5e07789189c75b4d3b804
bangms <uubang@knou.ac.kr>
2023-02-01 15:08:55 +0900

    puppeteer로 영화 평점 정보 크롤링

--- lecture/index.js
+++ lecture/index.js
@@ -1,4 +1,5 @@
 const parse = require('csv-parse/lib/sync');
+const { compareDocumentPosition } = require('domutils');
 const fs = require('fs'); 
 /*
     puppeteer
@@ -14,23 +15,30 @@ const csv = fs.readFileSync('csv/data.csv'); // readFileSync 파일을 읽어들
 const records = parse(csv.toString('utf-8')); // csv-parse의 parse 메서드가 문자열을 2차원 배열로 바꿈
 
 const crawler = async () => { 
-    const browser = await puppeteer.launch({ headless: false });
-    const [page, page2] = await Promise.all([
-        browser.newPage(),
-        browser.newPage(),
-    ]);
-    await Promise.all([
-        page.goto('https://www.naver.com/'),
-        page2.goto('https://google.com'),
-    ]);
-    await Promise.all([
-        page.waitForTimeout(3000),
-        page2.waitForTimeout(1000),
-    ]);
-    await page.close();
-    await page2.close();
-    await browser.close();
-    // 생성한 페이지와 브라우저는 반드시 close() 하는 것이 메모리에 좋음 
+    // UnhandledPromiseRejectionWarning Error: Page crashed!
+    // promise가 에러가 나는 경우를 잡아야 하기 때문에 try catch문 사용
+    try {
+        const browser = await puppeteer.launch({ headless: false });
+        await Promise.all(records.map(async (r, i) => {
+            try {
+                const page = await browser.newPage(); // 동시에 10개가 열림
+                await page.goto(r[1]); // r[1] : 영화 링크 주소 // Promise.all 이기 때문에 순서가 엉킬 수 있음
+                const scoreEl = await page.$('.score.score_left .star_score'); // 띄워진 페이지에서 해당 태그 찾기
+                if (scoreEl) { // 크롤러가 에러가 생겨서 태그를 못찾는 경우가 발생할 수 있음(정확히는 태그 핸들러라서 태그 속성을 그대로 사용할 수 없음)
+                    const text = await page.evaluate(tag => tag.textContent, scoreEl);
+                    console.log(r[0], '평점', text.trim());
+                } 
+                await page.waitForTimeout(3000);
+                await page.close();
+            } catch (e) {
+                console.error(e);
+            }
+        }));
+        await browser.close();
+        // 생성한 페이지와 브라우저는 반드시 close() 하는 것이 메모리에 좋음 
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 crawler();
