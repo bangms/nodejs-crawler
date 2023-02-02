4e8447519b9f75e40ef3d5a399ffc621ff77aad3
bangms <uubang@knou.ac.kr>
2023-02-02 11:18:30 +0900

    axios로 이미지 저장하기

--- lecture/index.js
+++ lecture/index.js
@@ -1,10 +1,27 @@
 const xlsx = require('xlsx');
 const puppeteer = require('puppeteer');
+const axios = require('axios');
+const fs = require('fs');
 const add_to_sheet = require('./add_to_sheet');
 
 const workbook = xlsx.readFile('xlsx/data.xlsx');
 const ws = workbook.Sheets.영화목록;
 const records = xlsx.utils.sheet_to_json(ws); 
+
+// 기존에 폴더가 있는 경우 충돌나서 에러가 나기 때문에 먼저 있는지 없는지 먼저 검사를 하고 없을 경우 폴더를 추가
+fs.readdir('screenshot', (err) => { // readdir은 비동기 방식 
+    if (err) {
+        console.error('screenshot 폴더가 없어 screenshot 폴더를 생성합니다.');
+        fs.mkdirSync('screenshot'); // Sync가 있으면 동기식으로 동작하는데 readdir이 비동기방식이기 때문에 mkdir로 하더라도 여기서는 큰 차이가 없음
+    }
+})
+
+fs.readdir('poster', (err) => {
+    if (err) {
+        console.error('poster 폴더가 없어 poster 폴더를 생성합니다.');
+        fs.mkdirSync('poster');
+    }
+})
 
 const crawler = async () => { 
     try {
@@ -16,16 +33,29 @@ const crawler = async () => {
             await page.goto(r.링크); 
             // const 태그핸들러 = await page.$(선택자);
             console.log(await page.evaluate('navigator.userAgent'));
-            const text = await page.evaluate(() => {
-                const scroe = document.querySelector('.score.score_left .star_score');
-                if (scroe) {
-                    return scroe.textContent;
+            const result = await page.evaluate(() => {
+                const scoreEl = document.querySelector('.score.score_left .star_score');
+                let score = '';
+                if (scoreEl) {
+                    score = score.textContent;
                 }
+                const imgEl = document.querySelector('.poster img');
+                let img = '';
+                if (imgEl) {
+                    img = imgEl.src;
+                }
+                return { score, img };
             });
-            if (text) {
-                console.log(r.제목, '평점', text.trim());
+            if (result.score) {
+                console.log(r.제목, '평점', result.score.trim());
                 const newCell = 'C' + (i + 2);
-                add_to_sheet(ws, newCell, 'n', parseFloat(text.trim()));
+                add_to_sheet(ws, newCell, 'n', parseFloat(result.score.trim()));
+            }
+            if (result.img) {
+                const imgResult = await axios.get(result.img.replace(/\?.*$/, ''), { // replace(/\?.*$/, '') -> 쿼리 스트링을 제거하는 정규표현식 ?부터 끝부분까지 제거 // ?.+$에서 .은 모든 단어 +는 한 개 이상, $는 끝을 의미함
+                    responseType: 'arraybuffer', // arraybuffer : buffer가 연속적으로 들어있는 자료 구조
+                });
+                fs.writeFileSync(`poster/${r.제목}.jpg`, imgResult.data);
             }
             await page.waitForTimeout(1000); // 시간을 줄이고 늘려보면서 차단 당하는지 안하는지 테스트 필요
         }
